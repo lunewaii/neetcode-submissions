5c6f27ca01e61f9899aaebc5fd574e4b51289773
rebeca <rebecacarneiropaulaoficial@gmail.com>
2026-09-23 11:12:20 -0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 15
+print(type(variable))
+variable = 15.4
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "oi"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
